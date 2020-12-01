a34806371a0c0fd428331ba73f61ad1c432c75f9
Daniel Brandao <dannnbrandao@gmail.com>
2020-12-01 01:44:47 -0300

    Add Texts, RaisedButton e cores no layout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,15 +6,45 @@ class HomePage extends StatelessWidget {
     final mediaQuery = MediaQuery.of(context).size;
 
     return Scaffold(
-      //backgroundColor: ,
+      // backgroundColor: Colors.blue,
       body: Container(
+        // Pegar todo tamanho da tela
         height: mediaQuery.height,
         width: mediaQuery.width,
+        color: Colors.red[300],
 
         //
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/logo.png'),
+            Container(
+              child: Image.asset(
+                'assets/images/logo.png',
+                color: Colors.white,
+                scale: 5,
+              ),
+              color: Colors.green,
+              //padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.only(top: 10.0, bottom: 20),
+            ),
+
+            //
+            //SizedBox(height: 20),
+            Text(
+              "Location Changer",
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+
+            //
+            SizedBox(height: 10),
+            Text(
+              "Plugin App for Tinder",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+
+            //
+            SizedBox(height: 50),
+            RaisedButton(onPressed: null),
           ],
         ),
       ),
